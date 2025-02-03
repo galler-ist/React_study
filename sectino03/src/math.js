1543ecef76858bb49e65bd0df9009ef30ad6e010
swghj <swghj@Naver.com>
2025-02-03 23:20:19 +0900

    add : Challenge Day 8

---
+++ sectino03/src/math.js
@@ -0,0 +1,17 @@
+// math module
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+// module.exports = {
+//   //   add: add,
+//   //   sub: sub,
+//   add,
+//   sub,
+// };
+
+// export { add, sub };
