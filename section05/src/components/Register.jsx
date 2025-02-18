0c7d40caa1532b1df76a4b923845133756057c72
swghj <swghj@Naver.com>
2025-02-19 07:28:01 +0900

    add : Challenge Day 12

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,52 +1,90 @@
-import { useState } from "react";
-
+import { useState, useRef } from "react";
 const Register = () => {
-  const [name, setName] = useState("");
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("");
+  //   const [name, setName] = useState("");
+  //   const [birth, setBirth] = useState("");
+  //   const [country, setCountry] = useState("");
+  //   const [bio, setBio] = useState("");
 
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  const countRef = useRef(0);
+  const inputRef = useRef();
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+
+  const onChange = (e) => {
+    // console.log(e.target.name, e.target.value);
+    countRef.current++;
+    console.log(countRef.current);
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
+  const onSubmit = () => {
+    if (input.name === "") {
+      // 이름을 입력하는 DOM 요소 포커스
+      inputRef.current.focus();
+    }
   };
+  //   const onChangeName = (e) => {
+  //     // setName(e.target.value);
+  //     setInput({ ...input, name: e.target.value });
+  //   };
 
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
+  //   const onChangeBirth = (e) => {
+  //     setInput({ ...input, birth: e.target.value });
+  //   };
 
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  };
+  //   const onChangeCountry = (e) => {
+  //     setInput({ ...input, country: e.target.value });
+  //   };
+
+  //   const onChangeBio = (e) => {
+  //     setInput({ ...input, bio: e.target.value });
+  //   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} placeholder={"이름"} />
-        {name}
+        <input
+          ref={inputRef}
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
+        {input.name}
       </div>
       <div>
-        <input value={birth} onChange={onChangeBirth} type="date" />
-        {birth}
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
+        {input.birth}
       </div>
 
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select name="country" value={input.country} onChange={onChange}>
           <option value="">===</option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
           <option value="uk">영국</option>
         </select>
-        {country}
+        {input.country}
       </div>
 
       <div>
-        <textarea value={bio} onChange={onChangeBio} />
-        {bio}
+        <textarea name="bio" value={input.bio} onChange={onChange} />
+        {input.bio}
       </div>
+
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
