83d0222f2506acac00d7bf045e7e41e754eb3a62
swghj <swghj@Naver.com>
2025-02-26 07:32:49 +0900

    add : Challenge Day 17 with Context(using and updating and Optimization)

---
+++ section11/src/App.jsx
@@ -0,0 +1,131 @@
+import {
+  useState,
+  useRef,
+  useReducer,
+  useCallback,
+  createContext,
+  useMemo,
+} from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+// import Exam from "./components/Exam";
+
+// mockdata는 app component가 리렌더링 될때마다 재생성 될 필요 없기 때문에
+// component 외부에 선언
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "청소하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
+export const TodoStateContext = createContext();
+export const TodoDispatchContext = createContext();
+
+function App() {
+  // const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  // const onCreate = (content) => {
+  //   dispatch({
+  //     type: "CREATE",
+  //     data: {
+  //       id: idRef.current++,
+  //       isDone: false,
+  //       content: content,
+  //       date: new Date().getTime(),
+  //     },
+  //   });
+  // };
+
+  // const onUpdate = (targetId) => {
+  //   dispatch({
+  //     type: "UPDATE",
+  //     targetId: targetId,
+  //   });
+  // };
+
+  // const onDelete = (targetId) => {
+  //   dispatch({
+  //     type: "DELETE",
+  //     targetId: targetId,
+  //   });
+  // };
+
+  const onCreate = useCallback((content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  }, []);
+
+  const onUpdate = useCallback((targetId) => {
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
+  }, []);
+
+  const onDelete = useCallback((targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  }, []);
+
+  const memoizedDisPatch = useMemo(() => {
+    return { onCreate, onUpdate, onDelete };
+  }, []);
+
+  return (
+    <div className="App">
+      {/* <Exam /> */}
+      <Header />
+      <TodoStateContext.Provider value={todos}>
+        <TodoDispatchContext.Provider value={memoizedDisPatch}>
+          <Editor />
+          <List />
+        </TodoDispatchContext.Provider>
+      </TodoStateContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ section11/src/components/TodoItem.jsx
@@ -0,0 +1,40 @@
+import "./TodoItem.css";
+import { memo, useContext } from "react";
+import { TodoDispatchContext } from "../App";
+
+const TodoItem = ({ id, isDone, content, date }) => {
+  const { onUpdate, onDelete } = useContext(TodoDispatchContext);
+
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
+  const onDeleteItem = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div className="TodoItem">
+      <input
+        checked={isDone}
+        onChange={onChangeCheckbox}
+        readOnly
+        type="checkbox"
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onDeleteItem}>삭제</button>
+    </div>
+  );
+};
+
+// const memoTodoItem = memo(TodoItem, (prevProps, nextProps) => {
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
+
+//   return true;
+// });
+
+export default memo(TodoItem);
